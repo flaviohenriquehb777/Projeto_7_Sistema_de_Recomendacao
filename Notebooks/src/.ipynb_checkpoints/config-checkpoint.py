9054df06a6b356727e1746eeaa038e5947bd80c6
Flávio Henrique Barbosa <109081925+flaviohenriquehb777@users.noreply.github.com>
2024-10-18 10:17:00 +0100

    777_ed_final

---
+++ Notebooks/src/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+PASTA_PROJETO = Path(__file__).resolve().parents[2]
+
+PASTA_DADOS = PASTA_PROJETO / "dados"
+
+# coloque abaixo o caminho para os arquivos de dados de seu projeto
+DADOS_BRUTOS = PASTA_DADOS / "Criando uma apresentação executiva.csv"
+DADOS_TRATADOS = PASTA_DADOS / "Criando uma apresentação executiva.parquet"
+
+# coloque abaixo o caminho para os arquivos de modelos de seu projeto
+PASTA_MODELOS = PASTA_PROJETO / "modelos"
+
+# coloque abaixo outros caminhos que você julgar necessário
+PASTA_RELATORIOS = PASTA_PROJETO / "relatorios"
+PASTA_IMAGENS = PASTA_RELATORIOS / "imagens"
